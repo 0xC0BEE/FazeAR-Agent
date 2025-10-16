e949ce9ce8547dcdaaf8d051e68838dcc461067b
0xC0BEE <0xc0bee@pm.me>
2025-10-16 20:16:04 +0530

    Updated .gitignore

--- apiKey.ts
+++
@@ -1,5 +0,0 @@
-// IMPORTANT: DO NOT COMMIT THIS FILE TO VERSION CONTROL (e.g., GitHub).
-// This file is for local development only and contains sensitive data.
-// Replace "YOUR_API_KEY_HERE" with your actual Gemini API key.
-
-export const GEMINI_API_KEY = "AIzaSyCGugvOfyj10flYRqMEAWj8FyjyZF-H35s";
